fd2ce9b7a48d3f3720efdc6cdf9eb25c61a9bbe2
maryalganes-cpu <mary.alganes@ciit.edu.ph>
2026-02-16 00:01:12 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿
+namespace table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number to \n see the multiplaction table: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= 10; i++)
+                Console.WriteLine(i + " x " + num + " = " + (i * num));
+        }
+    }
+}
